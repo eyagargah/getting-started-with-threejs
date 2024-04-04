64c9266aa8012642527bbf632c4114d388a7fef3
Eya Gargah <eyagargah@hotmail.com>
2024-04-04 23:24:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Robot Bobby was here');
+import * as THREE from "three"
+
+
+const w = window.innerWidth
+const h = window.innerHeight
+const renderer = new THREE.WebGLRenderer({ antialias : true})
+
+renderer.setSize(w ,h)
+
+document.body.appendChild(renderer.domElement)
+
+const fov = 75
+const  aspect = w/h
+const near = 0.1
+const far  = 10
+const camera = new THREE.PerspectiveCamera(fov , aspect , near , far)
+
+camera.position.z =2
+
+const scene = new THREE.Scene()
+const geo = new THREE.IcosahedronGeometry(1.0 , 2)
+const mat = new THREE.mesh
+
+renderer.render(scene , camera)
