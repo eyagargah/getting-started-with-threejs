19e0548aef40f27b8a869b920aa1e0cadea34082
Eya Gargah <eyagargah@hotmail.com>
2024-04-05 22:50:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,16 +18,29 @@ camera.position.z = 2;
 
 const scene = new THREE.Scene();
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshBasicMaterial({
-  color: 0xccff,
+const mat = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  flatShading : true
 });
 const mesh = new THREE.Mesh(geo, mat);
+
+const wireMat = new THREE.MeshBasicMaterial({
+    color:0xffffff,
+    wireframe:true
+})
+
+const wireMesh = new THREE.Mesh(geo,wireMat)
+wireMesh.scale.setScalar(1.001)
+mesh.add(wireMesh)
 scene.add(mesh);
+
+var light = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+
+scene.add(light);
 
 function animate(t=0) {
   requestAnimationFrame(animate);
-  
-  //mesh.scale.setScalar(Math.cos(t * 0.001)+1.0)
+  mesh.rotation.y = t*0.001
   renderer.render(scene, camera);
 }
 animate();
